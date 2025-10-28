56689ecc4280f93109fd90c41384f51a6331008d
mriskisyahputraa2 <riskideveloper2@gmail.com>
2025-10-28 11:26:00 +0700

    feat: Add logo image and update user info component to use photo_url

--- resources/js/components/user-info.tsx
+++ resources/js/components/user-info.tsx
@@ -6,7 +6,7 @@ export function UserInfo({
     user,
     showEmail = false,
 }: {
-    user: User;
+    user: User & { photo_url?: string };
     showEmail?: boolean;
 }) {
     const getInitials = useInitials();
@@ -14,7 +14,11 @@ export function UserInfo({
     return (
         <>
             <Avatar className="h-8 w-8 overflow-hidden rounded-full">
-                <AvatarImage src={user.avatar} alt={user.name} />
+                <AvatarImage
+                    key={user.photo_url}
+                    src={user.photo_url}
+                    alt={user.name}
+                />
                 <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
                     {getInitials(user.name)}
                 </AvatarFallback>
